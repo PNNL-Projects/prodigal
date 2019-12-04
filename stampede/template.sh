c1fccc11cfdc38c42166712c98e8653671477a3c
Ken Youens-Clark <kyclark@gmail.com>
2019-12-04 09:59:22 -0700

    cleanup

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-echo "QUERY                 \"${QUERY}\""
-echo "WRITE_PROT            \"${WRITE_PROT}\""
-echo "CLOSED_ENDS           \"${CLOSED_ENDS}\""
-echo "WRITE_NUCL            \"${WRITE_NUCL}\""
-echo "OUTPUT_FORMAT         \"${OUTPUT_FORMAT}\""
-echo "NS_AS_MASKED          \"${NS_AS_MASKED}\""
-echo "BYPASS_SHINE_DALGARNO \"${BYPASS_SHINE_DALGARNO}\""
-echo "PROCEDURE             \"${PROCEDURE}\""
-echo "WRITE_GENES           \"${WRITE_GENES}\""
-
 sh run.sh ${QUERY} ${WRITE_PROT} ${CLOSED_ENDS} ${WRITE_NUCL} ${OUTPUT_FORMAT} ${NS_AS_MASKED} ${BYPASS_SHINE_DALGARNO} ${PROCEDURE} ${WRITE_GENES}
