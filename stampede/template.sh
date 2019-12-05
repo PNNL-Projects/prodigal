583d1850fbd386149c2930898a1726c1814d1fe4
Ken Youens-Clark <kyclark@email.arizona.edu>
2019-12-05 11:32:33 -0600

    working

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,1 +1,8 @@
-sh run.sh ${QUERY} ${WRITE_PROT} ${CLOSED_ENDS} ${WRITE_NUCL} ${OUTPUT_FORMAT} ${NS_AS_MASKED} ${BYPASS_SHINE_DALGARNO} ${PROCEDURE} ${WRITE_GENES}
+IMG="/work/05066/imicrobe/singularity/prodigal-2.6.3.img"
+
+if [[ ! -e "$IMG" ]]; then
+    echo "Missing Singularity image \"$IMG\""
+    exit 1
+fi
+
+singularity exec $IMG run_prodigal -o "prodigal-out" ${QUERY} ${WRITE_PROT} ${CLOSED_ENDS} ${WRITE_NUCL} ${OUTPUT_FORMAT} ${NS_AS_MASKED} ${BYPASS_SHINE_DALGARNO} ${PROCEDURE} ${WRITE_GENES}
